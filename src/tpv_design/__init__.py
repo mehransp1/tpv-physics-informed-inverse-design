2f9f002848b58c7689006c945ede32dad3847a43
Mehran Sepah mansoor <167231527+mehransp1@users.noreply.github.com>
2026-09-10 17:28:19 -0400

    Expose material-library API

--- src/tpv_design/__init__.py
+++ src/tpv_design/__init__.py
@@ -1,10 +1,22 @@
 """Physics-based tools for multilayer TPV spectral-filter design."""
 
+from .materials import (
+    MaterialData,
+    OpticalMaterial,
+    available_materials,
+    list_materials,
+    load_material,
+)
 from .optics import multilayer_rt, unpolarized_rt, hemispherical_rt
 from .radiation import planck_spectral_exitance
 from .tpv import TPVMetrics, evaluate_tpv
 
 __all__ = [
+    "MaterialData",
+    "OpticalMaterial",
+    "available_materials",
+    "list_materials",
+    "load_material",
     "multilayer_rt",
     "unpolarized_rt",
     "hemispherical_rt",
